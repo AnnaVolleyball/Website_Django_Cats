2311a2e4ddb3c0e52a861dc658923df9b6365279
Anna_Lapshina <annavolleyball211@mail.ru>
2024-06-24 18:27:18 +0700

    Make API to get, edit and delete the task having user

--- catsite/catsite/urls.py
+++ catsite/catsite/urls.py
@@ -12,8 +12,10 @@ urlpatterns = [
     path('edit_task/<int:task_id>/', views.EditTask.as_view(), name='edit_task'),
     path('delete_task/<int:task_id>/', views.DeleteTask.as_view(), name='delete_task'),
     path('galery/', views.Galery.as_view(), name='galery'),
-    path('api/tasks/', api.TaskListView.as_view(), name="tasks_api"),
+
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh')
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/tasks/', api.TaskListView.as_view(), name="tasks_api"),
+    path('api/tasks/<int:pk>/', api.TaskDetailView.as_view(), name="task_api")
 
 ]
--- catsite/main/api.py
+++ catsite/main/api.py
@@ -4,12 +4,18 @@ from .models import Task
 from .serializers import TaskSerializer
 
 
-class TaskListView(generics.ListAPIView):
+class TaskListView(generics.ListCreateAPIView):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
     permission_classes = [IsAuthenticated]
 
-    # def get_queryset(self):
-    #     # user = self.request.user
-    #     # return Task.objects.filter(user=user)
-    #     return Task.objects.all()
+
+class TaskDetailView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = TaskSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Task.objects.filter(user=user)
+
+
